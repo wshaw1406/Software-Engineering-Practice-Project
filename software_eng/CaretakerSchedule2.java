e090d94a24daf1d648f4a3b3224898c49cbffd95
jordanm84 <33003400+jordanm84@users.noreply.github.com>
2018-03-27 16:55:23 +0100

    Schdeule2
    Button added to the table

--- software_eng/CaretakerSchedule2.java
+++ software_eng/CaretakerSchedule2.java
@@ -1,10 +1,14 @@
 package software_eng;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import java.sql.*;
 import java.util.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 
 public class CaretakerSchedule2 extends JFrame{
@@ -49,16 +53,89 @@ public class CaretakerSchedule2 extends JFrame{
 		frame.getContentPane().add(scrollPane_1);
 		
 		table = new JTable();
-		table.setEnabled(false);
 		scrollPane_1.setViewportView(table);
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{"7:12-8:00", "Test"},
-				{"8:00-9:00", "dsfdsf"},
+				{"7:12-8:00", "Test","notes"},
+				{"8:00-9:00", "dsfdsf","notes2"},
 			},
 			new String[] {
-				"Time", "Desc."
+				"Time", "Desc.", "Notes"
 			}
 		));
+		table.getColumnModel().getColumn(2).setCellRenderer(new ButtonRenderer());;
+		table.getColumnModel().getColumn(2).setCellEditor(new ButtonEditor(new JTextField()));
+	}
+}
+
+class ButtonRenderer extends JButton implements TableCellRenderer
+{
+
+	public ButtonRenderer() {
+		setOpaque(true);
+	}
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object obj, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		
+		setText((obj==null) ? "":obj.toString());
+		
+		return this;
+	} 
+}
+
+class ButtonEditor extends DefaultCellEditor
+{
+	protected JButton btn;
+	private String lbl;
+	private Boolean clicked;
+	
+	public ButtonEditor(JTextField txt) {
+		super(txt);
+		
+		btn = new JButton();
+		btn.setOpaque(true);
+		
+		
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				fireEditingStopped();
+			}
+			
+		});
+
+	}
+	
+	@Override
+	public Component getTableCellEditorComponent(JTable table, Object obj, boolean isSelected, int row, int column) {
+		lbl=(obj==null) ? "":obj.toString();
+		btn.setText(lbl);
+		clicked = true;
+		
+		return btn;
+	}
+	
+	@Override
+	public Object getCellEditorValue() {
+
+		if(clicked) {
+			JOptionPane.showMessageDialog(btn, lbl+" Clicked");
+		}
+		clicked = false;
+		return new String(lbl);
+	}
+	
+	@Override
+	public boolean stopCellEditing() {
+		clicked = false;
+		return super.stopCellEditing();
+	}
+	
+	@Override
+	protected void fireEditingStopped() {
+		// TODO Auto-generated method stub
+		super.fireEditingStopped();
 	}
 }
