c034f752ffe038622cfaef74def9fcbaeb291046
danielparr <36413897+danielparr@users.noreply.github.com>
2018-03-27 15:03:09 +0100

    \( -_-)>

---
+++ adminGUI.java
@@ -0,0 +1,64 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JLabel;
+
+public class adminGUI {
+
+	private JFrame frmAdministration;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					adminGUI window = new adminGUI();
+					window.frmAdministration.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public adminGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmAdministration = new JFrame();
+		frmAdministration.setTitle("Administration");
+		frmAdministration.setBounds(100, 100, 580, 381);
+		frmAdministration.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAdministration.getContentPane().setLayout(null);
+		
+		JButton btnLogout = new JButton("Log-out");
+		btnLogout.setBounds(465, 11, 89, 23);
+		frmAdministration.getContentPane().add(btnLogout);
+		
+		JButton btnUsers = new JButton("User Information");
+		btnUsers.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnUsers.setBounds(95, 129, 127, 61);
+		frmAdministration.getContentPane().add(btnUsers);
+		
+		JButton btnTaskInformation = new JButton("Task Information");
+		btnTaskInformation.setBounds(326, 129, 127, 61);
+		frmAdministration.getContentPane().add(btnTaskInformation);
+	}
+
+}
---
+++ taskInformation.java
@@ -0,0 +1,126 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+
+public class taskInformation {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					taskInformation window = new taskInformation();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public taskInformation() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 571, 405);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("Add New Task");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		label.setBounds(319, 115, 92, 14);
+		frame.getContentPane().add(label);
+		
+		JButton button = new JButton("Add");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		button.setBounds(430, 110, 92, 23);
+		frame.getContentPane().add(button);
+		
+		JButton button_1 = new JButton("Edit");
+		button_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		button_1.setBounds(430, 160, 92, 23);
+		frame.getContentPane().add(button_1);
+		
+		JLabel label_1 = new JLabel("Edit Task");
+		label_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		label_1.setBounds(319, 165, 92, 14);
+		frame.getContentPane().add(label_1);
+		
+		JButton button_2 = new JButton("Remove");
+		button_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		button_2.setBounds(430, 209, 92, 23);
+		frame.getContentPane().add(button_2);
+		
+		JLabel label_2 = new JLabel("Remove Task");
+		label_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		label_2.setBounds(319, 214, 92, 14);
+		frame.getContentPane().add(label_2);
+		
+		JLabel label_3 = new JLabel("Task Report");
+		label_3.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		label_3.setBounds(319, 260, 92, 23);
+		frame.getContentPane().add(label_3);
+		
+		JButton btnReport = new JButton("Report");
+		btnReport.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnReport.setBounds(430, 259, 89, 23);
+		frame.getContentPane().add(btnReport);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 50, 259, 306);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 239, 284);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"ID", "Name"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JLabel lblTasks = new JLabel("Tasks");
+		lblTasks.setBounds(127, 25, 46, 14);
+		frame.getContentPane().add(lblTasks);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(456, 11, 89, 23);
+		frame.getContentPane().add(btnBack);
+	}
+
+}
