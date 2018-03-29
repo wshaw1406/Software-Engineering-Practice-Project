85fefe74b8e4af0151dac289b5d46743a4477e47
w16013867 <35560370+w16013867@users.noreply.github.com>
2018-03-29 14:38:56 +0100

    Add files via upload

--- TaskLogging.java
+++ TaskLogging.java
@@ -13,13 +13,14 @@ import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 import java.util.Calendar;
 import javax.swing.SpinnerDateModel;
 import javax.swing.SwingUtilities;
 import javax.swing.text.DateFormatter;
-
-
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
 
 public class TaskLogging {
 
@@ -60,7 +61,11 @@ public class TaskLogging {
 		frmTaskLogging.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTaskLogging.getContentPane().setLayout(null);
 		
+		//CONNECT TO DATABASE
+		//CONNECT TO PREVIOUS PAGE
+		
 		JLabel lblCaretakerName = new JLabel("Caretaker Name:");
+		//GET CARETAKER NAME WHO MEANT TO DO IT
 		lblCaretakerName.setBounds(25, 28, 117, 14);
 		frmTaskLogging.getContentPane().add(lblCaretakerName);
 		
@@ -72,20 +77,39 @@ public class TaskLogging {
 		lblAdditionalComments.setBounds(25, 78, 134, 14);
 		frmTaskLogging.getContentPane().add(lblAdditionalComments);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(171, 73, 206, 105);
-		frmTaskLogging.getContentPane().add(textArea);
-		
 		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to Log this task?", "Confirm",
+				        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+				    if (response == JOptionPane.NO_OPTION) {
+				      System.out.println("");
+				    } else if (response == JOptionPane.YES_OPTION) {
+				      System.out.println("Task has been logged");
+				    //UPDATE DATABASE
+				    } 
+			}
+		});
+	    
+
 		btnSubmit.setBounds(168, 181, 89, 23);
 		frmTaskLogging.getContentPane().add(btnSubmit);
 		
 		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}
+		});
 		btnCancel.setBounds(292, 181, 89, 23);
 		frmTaskLogging.getContentPane().add(btnCancel);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerDateModel(new Date(1521619200000L), new Date(1521619200000L), new Date(1521662400000L), Calendar.HOUR_OF_DAY));
+		Date datenow = Calendar.getInstance().getTime();
+		SpinnerDateModel smb = new SpinnerDateModel(datenow, null, null, Calendar.HOUR_OF_DAY);
+		spinner.setModel(smb);
+		JSpinner.DateEditor d = new JSpinner.DateEditor(spinner, "dd-MM-yyyy HH:mm");
+		spinner.setEditor(d);
 		spinner.setBackground(new Color(240, 240, 240));
 		spinner.setBounds(171, 49, 134, 22);
 		frmTaskLogging.getContentPane().add(spinner);
@@ -94,9 +118,14 @@ public class TaskLogging {
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Caretaker 1", "Caretaker 2", "Caretaker 3", "Caretaker 4", "Caretaker 5", "Caretaker 6"}));
 		comboBox.setBounds(171, 24, 134, 22);
 		frmTaskLogging.getContentPane().add(comboBox);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(171, 78, 210, 90);
+		frmTaskLogging.getContentPane().add(scrollPane);
+		
+		JTextArea txtrTypeAnyComment = new JTextArea();
+		scrollPane.setViewportView(txtrTypeAnyComment);
+		txtrTypeAnyComment.setText("#Type any comments here");
+		txtrTypeAnyComment.setLineWrap(true);
 	}
-
-
-
-
 } 
