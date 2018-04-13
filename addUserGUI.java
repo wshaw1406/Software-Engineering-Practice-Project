b2577d6ed016644d39307faed1766c9cb3382a06
danielparr <36413897+danielparr@users.noreply.github.com>
2018-04-13 12:46:20 +0100

    aaaaaaaaaaaaaa

--- addUserGUI.java
+++ addUserGUI.java
@@ -20,6 +20,10 @@ import java.awt.event.KeyEvent;
 import java.time.DayOfWeek;
 import java.time.Month;
 import java.util.Date;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseMotionAdapter;
 
 public class addUserGUI {
 
@@ -77,7 +81,7 @@ public class addUserGUI {
 		frmAddNewUser.getContentPane().add(validation);		
 		
 		JLabel validation2 = new JLabel("");
-		validation2.setBounds(182, 291, 252, 14);
+		validation2.setBounds(182, 291, 516, 14);
 		frmAddNewUser.getContentPane().add(validation2);
 		
 		textField = new JTextField();
@@ -85,7 +89,6 @@ public class addUserGUI {
 			@Override
 			public void keyTyped(KeyEvent e) {
 				char c=e.getKeyChar();
-				    if(!(Character.isAlphabetic(c) || (c==KeyEvent.VK_BACK_SPACE)|| c==KeyEvent.VK_DELETE ))
 				        e.consume();
 			}
 		});		
@@ -129,7 +132,22 @@ public class addUserGUI {
 		
 		JLabel lblGender = new JLabel("* Gender");
 		lblGender.setBounds(59, 208, 76, 14);
-		frmAddNewUser.getContentPane().add(lblGender);
+		frmAddNewUser.getContentPane().add(lblGender);		
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", "1975", "1974", "1973", "1972", "1971", "1970", "1969", "1968", "1967", "1966", "1965", "1964", "1963", "1962", "1961", "1960", "1959", "1958", "1957", "1956", "1955", "1954", "1953", "1952", "1951", "1950", "1949", "1948", "1947", "1946", "1945", "1944", "1943", "1942", "1941", "1940", "1939", "1938", "1937", "1936", "1935", "1934", "1933", "1932", "1931", "1930", "1929", "1928", "1927", "1926", "1925", "1924", "1923", "1922", "1921", "1920", "1919", "1918", "1917", "1916", "1915", "1914", "1913", "1912", "1911", "1910", "1909", "1908", "1907", "1906", "1905", "1904", "1903", "1902", "1901", "1900"}));
+		comboBox_2.setBounds(288, 130, 64, 20);
+		frmAddNewUser.getContentPane().add(comboBox_2);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}));
+		comboBox_3.setBounds(236, 130, 44, 20);
+		frmAddNewUser.getContentPane().add(comboBox_3);
+		
+		JComboBox comboBox_4 = new JComboBox();
+		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		comboBox_4.setBounds(182, 130, 44, 20);
+		frmAddNewUser.getContentPane().add(comboBox_4);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setToolTipText("");
@@ -138,13 +156,13 @@ public class addUserGUI {
 		frmAddNewUser.getContentPane().add(comboBox_1);
 		
 		textField_2 = new JTextField();
-		textField_2.addKeyListener(new KeyAdapter() {
+		textField_2.addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
-			public void keyTyped(KeyEvent e) {
-				if (textField_2.getText().length() >= 12)
-		            e.consume();
+			public void mouseMoved(MouseEvent arg0) {
+				textField_2.setText(textField_1.getText()+comboBox_4.getSelectedItem());
 			}
 		});		
+		textField_2.setEditable(false);
 		textField_2.setColumns(10);
 		textField_2.setBounds(182, 239, 170, 20);
 		frmAddNewUser.getContentPane().add(textField_2);
@@ -158,8 +176,8 @@ public class addUserGUI {
 		passwordField.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent e) {
-				if (passwordField.getText().length() <= 5){
-				     validation2.setText("Password must be more than 6 characters");
+				if (passwordField.getText().length() < 5){
+				     validation2.setText("Password must be 6 or more characters");
 			} else {
 				validation2.setText("");
 			}
@@ -193,48 +211,31 @@ public class addUserGUI {
 		
 		JLabel lblTheseFields = new JLabel("* These fields are required");
 		lblTheseFields.setBounds(10, 35, 254, 14);
-		frmAddNewUser.getContentPane().add(lblTheseFields);		
+		frmAddNewUser.getContentPane().add(lblTheseFields);
 		
-		JLabel validation3 = new JLabel("");
-		validation3.setBounds(182, 329, 347, 14);
-		frmAddNewUser.getContentPane().add(validation3);
+		JLabel rePassVal = new JLabel("");
+		rePassVal.setBounds(182, 329, 398, 14);
+		frmAddNewUser.getContentPane().add(rePassVal);
 		
 		passwordField_1 = new JPasswordField();
-		String p1 = passwordField_1.getText();
 		passwordField_1.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyTyped(KeyEvent e) {
-				if (p2 == p1){
-				     validation3.setText("Passwords do not match");
-			} else {
-				validation3.setText("");
+			public void keyReleased(KeyEvent e) {
+				if (passwordField.getText().equals(passwordField_1.getText())){
+					rePassVal.setText("");
+				}
+				else{
+					rePassVal.setText("Passwords must match");
+				}
 			}
-			}
-		});		
+		});
 		passwordField_1.setBounds(182, 305, 170, 20);
-		frmAddNewUser.getContentPane().add(passwordField_1);	
-		
-		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", "1975", "1974", "1973", "1972", "1971", "1970", "1969", "1968", "1967", "1966", "1965", "1964", "1963", "1962", "1961", "1960", "1959", "1958", "1957", "1956", "1955", "1954", "1953", "1952", "1951", "1950", "1949", "1948", "1947", "1946", "1945", "1944", "1943", "1942", "1941", "1940", "1939", "1938", "1937", "1936", "1935", "1934", "1933", "1932", "1931", "1930", "1929", "1928", "1927", "1926", "1925", "1924", "1923", "1922", "1921", "1920", "1919", "1918", "1917", "1916", "1915", "1914", "1913", "1912", "1911", "1910", "1909", "1908", "1907", "1906", "1905", "1904", "1903", "1902", "1901", "1900"}));
-		comboBox_2.setBounds(288, 130, 64, 20);
-		frmAddNewUser.getContentPane().add(comboBox_2);
-		
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}));
-		comboBox_3.setBounds(236, 130, 44, 20);
-		frmAddNewUser.getContentPane().add(comboBox_3);
-		
-		JComboBox comboBox_4 = new JComboBox();
-		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
-		comboBox_4.setBounds(182, 130, 44, 20);
-		frmAddNewUser.getContentPane().add(comboBox_4);
-
+		frmAddNewUser.getContentPane().add(passwordField_1);			
 
 		String day = String.valueOf(comboBox_4.getSelectedItem());
 		String month= String.valueOf(comboBox_3.getSelectedItem());
 		String year= String.valueOf(comboBox_2.getSelectedItem());
 		String DOB= year+ "" + month + ""+ day;
-		}
+		}	
 	}
 
