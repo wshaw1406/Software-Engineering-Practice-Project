48fdeb089960fd5837521fe268781ccbf0a67637
w16013867 <35560370+w16013867@users.noreply.github.com>
2018-04-18 20:01:33 +0100

    Add files via upload

--- CaretakerSchedule2.java
+++ CaretakerSchedule2.java
@@ -1,5 +1,3 @@
-package software_eng;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,14 +7,20 @@ import java.text.SimpleDateFormat;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableRowSorter;
 import java.util.*;
 
-
 public class CaretakerSchedule2 extends JFrame{
-
+	
+	//Define private and public variables 
 	private JFrame frame;
 	public static JTable table;
 	public static DefaultTableModel model;
+	//Sorts rows in table
+	private void sort() {
+		TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<DefaultTableModel> (model);
+		table.setRowSorter(sorter);
+	}
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -31,7 +35,10 @@ public class CaretakerSchedule2 extends JFrame{
 	}
 	
 	public CaretakerSchedule2() {
+		//Calls initialize function
 		initialize();
+		//Shows the frame
+		frame.setVisible(true);
 	}
 	
 	private void initialize() {
@@ -41,9 +48,12 @@ public class CaretakerSchedule2 extends JFrame{
 		frame.setTitle("Caretaker Schedule");
 		frame.getContentPane().setLayout(null);
 		
+		//JButton for undo
 		JButton btnUndo = new JButton("Undo");
+		//ActionListener, when button is clicked
 		btnUndo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//Hides frame and creates new Main
 				frame.hide();
 				new Main();
 			}
@@ -51,92 +61,96 @@ public class CaretakerSchedule2 extends JFrame{
 		btnUndo.setBounds(0, 366, 330, 49);
 		frame.getContentPane().add(btnUndo);
 		
+		//JButton for Complete
 		JButton btnComplete = new JButton("Complete");
+		//ActionListener for when button is clicked
 		btnComplete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//Hides the frame
 				frame.hide();
-				model = (DefaultTableModel) table.getModel();
-				for(int i = 0; i < model.getRowCount(); i++) {
-					//Boolean value = (Boolean) table.getModel().getValueAt(i, 3);
-					for(Task task : Main.tasks)
-				    {
-						//for each task that matches the tasks in the caretakers timetable
-						if(task.getTaskTitle() == table.getModel().getValueAt(i, 1).toString() ) {
-							new TaskLogging();
-							task.setTaskCompleted(true);
-							System.out.println(task.getTaskCompleted());
-						}
-				    }
-				}
+				//Creates TaskLogging 
+				new TaskLogging();
+
 				// UPDATE DB WITH EACH ARRAY LIST ITEM
 
 
 			}
 		});
-		
-		frame.setVisible(true);
 		btnComplete.setBounds(330, 366, 330, 49);
 		frame.getContentPane().add(btnComplete);
 		
+		//JScrollPane for the table, so iff too much data scroll bar can be used
 		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(123, 86, 343, 214);
+		scrollPane_1.setBounds(101, 95, 463, 214);
 		frame.getContentPane().add(scrollPane_1);
 		
+		//Jtable table
 		table = new JTable();		
 		scrollPane_1.setViewportView(table);
 		
+		//Default table
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
-				"Time", "Time allocated", "Title", "Notes", "Completed"
+				"Time", "Priority", "Title", "Time Allocated", "Type"
 			}
 		) {
-			Class[] columnTypes = new Class[] {
-				Object.class, Object.class, Object.class, Object.class, Boolean.class
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false
 			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
 			}
 		});
+		table.getColumnModel().getColumn(3).setPreferredWidth(103);
 		
-		
-		
-		JButton btnAssignNewTasks = new JButton("Assign new tasks");
+		//JButton for Assign New Tasks	
+		JButton btnAssignNewTasks = new JButton("Assign New Tasks");
+		//When button is clicked
 		btnAssignNewTasks.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//Creates TaskAllocation
 				new TaskAllocation();
+				//Hides frame
 				frame.hide();
+
 			}
 		});
-		btnAssignNewTasks.setBounds(187, 333, 213, 25);
+		btnAssignNewTasks.setBounds(225, 328, 213, 25);
 		frame.getContentPane().add(btnAssignNewTasks);
 		
-		JButton btnNewButton = new JButton("Home");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.hide();
-				new adminGUI();
-			}
-		});
-		btnNewButton.setBounds(56, 34, 97, 25);
-		frame.getContentPane().add(btnNewButton);
-		
+		//JButton for Completed Tasks
 		JButton btnEditCompletedTask = new JButton("Completed Tasks");
+		//When clicked
 		btnEditCompletedTask.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				//Hides frame
 				frame.hide();
-				
+				/////////////////////////////////////////////////////////////////////////////////////////////////
 			}
 		});
-		btnEditCompletedTask.setBounds(554, 13, 97, 25);
+		btnEditCompletedTask.setBounds(503, 13, 148, 25);
 		frame.getContentPane().add(btnEditCompletedTask);
 		
-		table.getColumnModel().getColumn(2).setCellRenderer(new ButtonRenderer());;
-		table.getColumnModel().getColumn(2).setCellEditor(new ButtonEditor(new JTextField()));
+		//JButton for Help
+		JButton btnHelp = new JButton("Help");
+		btnHelp.addActionListener(new ActionListener() {
+			//When clicked, displays message, telling user what to do
+			public void actionPerformed(ActionEvent arg0) {
+				Component frame = null;
+				//Opens JOptionPane
+				JOptionPane.showMessageDialog(frame,
+						"To Log a task: Select a task, then press 'Complete', "
+									+ "\nTo view Completed tasks: Select 'Completed Task', "
+									+ "\nTo add more tasks to your schedule, use 'Assign New Tasks',"
+									+ "\n then, tick the tasks you want and press 'submit'.");
+			}		
+		});
+		btnHelp.setBounds(12, 13, 97, 25);
+		frame.getContentPane().add(btnHelp);
 		
-		frame.setVisible(true);
-
+		//Table for caretakers tasks
 		model = (DefaultTableModel) table.getModel();
 		
 		String startTime = "9:15";
@@ -150,17 +164,23 @@ public class CaretakerSchedule2 extends JFrame{
 		 Calendar cal = Calendar.getInstance();
 		 cal.setTime(d);
 		 String newTime = df.format(cal.getTime());
-		 		
-		//If task is assigned to use show here
-	
+		 			
+		//For loop, runs through the tasks in Main
 		for(Task task : Main.tasks)
 	    {
+			//If, taskAssigned is true, run code
 			if(task.getTaskAssigned() == true) {
-				model.addRow(new Object[]{newTime ,task.getTaskTime(), task.getTaskTitle(), "notes", task.getTaskCompleted()});
+				//If, taskCompleted is false, run code
+				if(task.getTaskCompleted() == false) {
+					//Fills in table with respective data
+				model.addRow(new Object[]{newTime, task.getTaskPriority(), task.getTaskTitle(), task.getTaskTime(), task.getTaskType() });
+				}
 			}
 			cal.add(Calendar.MINUTE, task.getTaskTime());
 			newTime = df.format(cal.getTime());
 	    }
+		// Runs sort function
+		sort();
 	}
 }
 
@@ -214,51 +234,7 @@ class ButtonEditor extends DefaultCellEditor
 		return btn;
 	}
 	
-	@Override
-	public Object getCellEditorValue() {
-		
-		int column1 = 1;
-		int row1 = 1;
-		String value = "";
-		
-		if(clicked) {
-			System.out.println(btn.getName());
-
-			if(lbl == "notes") {
-				row1 = CaretakerSchedule2.table.getSelectedRow();
-				value = CaretakerSchedule2.table.getModel().getValueAt(row1, column1).toString();
-				for(Task task : Main.tasks)
-			    {
-					if(task.getTaskTitle() == value) {
-						JOptionPane.showMessageDialog(btn, task.getTaskNotes());
-					}
-			    }
-			}
-			else if(lbl == "assign") {
-				row1 = TaskAllocation.table.getSelectedRow();
-				value = TaskAllocation.table.getModel().getValueAt(row1, column1).toString();
-				for(Task task : Main.tasks)
-			    {
-					if(task.getTaskTitle() == value) {
-						JOptionPane.showMessageDialog(btn, "Task Assigned");
-						task.setTaskAssigned(false);
-					}
-			    }
-			}
-			else if(lbl == "note") {
-				row1 = TaskAllocation.table.getSelectedRow();
-				value = TaskAllocation.table.getModel().getValueAt(row1, column1).toString();
-				for(Task task : Main.tasks)
-			    {
-					if(task.getTaskTitle() == value) {
-						JOptionPane.showMessageDialog(btn, task.getTaskNotes());
-					}
-			    }
-			}
-		}
-		clicked = false;
-		return new String(lbl);
-	}
+	
 	
 	@Override
 	public boolean stopCellEditing() {
