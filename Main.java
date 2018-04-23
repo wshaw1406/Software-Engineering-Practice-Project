e726f39b07d3a0457afba2258fba3467af5fb0b3
jordanm84 <33003400+jordanm84@users.noreply.github.com>
2018-04-23 18:28:46 +0100

    k

--- Main.java
+++ Main.java
@@ -9,14 +9,16 @@ import java.util.*;
  */
 public class Main {
 	
-	// public static ArrayList<Task> tasks = new ArrayList<>();
+	public static ArrayList<Task> tasks = new ArrayList<>();
+	public static ArrayList<User> users = new ArrayList<>();
+	public static User user;
     
     public static void main(String args[]) {
     Database db = new Database();
-    //	tasks = (ArrayList<Task>) db.pullTasks();
-	//	new CaretakerSchedule2();
+    tasks = (ArrayList<Task>) db.pullTasks();
+    users = (ArrayList<User>) db.pullUsers();
+	//new CaretakerSchedule2();
     //	User user = db.pullSingleUser("tweed7");
-    //	System.out.println(user.getGender());
-    new taskInformation();
+    new loginGUI();
    }
 }
